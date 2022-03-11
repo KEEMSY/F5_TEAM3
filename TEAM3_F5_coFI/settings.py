325e1a7106c8f12374cac9ee3028f7678e2fe420
JerryKim <ecec1023@gmail.com>
2022-03-11 21:25:29 +0900

    Revert "추가 - 세팅스.py 에 실수 aaaaaa 추가 (삭제할 예정)"
    This reverts commit 74e3c5e1ef22d9d8c89b3193e39ee3121e61f88e.

--- TEAM3_F5_coFI/settings.py
+++ TEAM3_F5_coFI/settings.py
@@ -17,7 +17,7 @@ env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
 )
-aaaaaaa
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
