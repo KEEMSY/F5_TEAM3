eec8f812dc65bccf7aaba715f27c273e13b31928
sy <phzzz@naver.com>
2022-03-11 15:09:10 +0900

    project setting 1

---
+++ TEAM3_F5_coFI/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for TEAM3_F5_coFI project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TEAM3_F5_coFI.settings')
+
+application = get_wsgi_application()
