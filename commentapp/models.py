c3f9516682a1bdd24bf6e27232f4d81967ca25ce
sy <phzzz@naver.com>
2022-03-17 23:39:11 +0900

    add BaseModel and Comment

--- commentapp/models.py
+++ commentapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+from TEAM3_F5_coFI.models import BaseModel
+
+
+class Comment(BaseModel):
+
+    class Meta:
+        db_table = "comments"
+
+    def __str__(self):
+        return self.writer
+
+
+    article = models.TextField()
+    writer = models.TextField()
+    content = models.TextField()
+
+
---
+++ commentapp/tests/test_comment.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+
+
+# Given
+# When
+# Then
+# expect
+
+class TestView(TestCase):
+    def test_create_comment(self):
+        return 0
