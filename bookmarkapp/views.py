ff9dce05547dccd813ef02ad520865fa29e22b90
JerryKim <ecec1023@gmail.com>
2022-03-17 15:18:14 +0900

    수정 - 각 앱의 urls.py, views.py  / header.html,head.html 경로 수정해서 각각 페이지 연결되게 해놓음.

--- bookmarkapp/views.py
+++ bookmarkapp/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from bookmarkapp.models import Bookmark
 
 # Create your views here.
+
+
+def show_mypage(request):
+    return render(request,'userapp/mypage.html')
--- userapp/views.py
+++ userapp/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
+# from user.models import UserModel
 
-# Create your views here.
+# AWS json 가져오기 위한 장치
+import os
+import json
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+with open(os.path.join(BASE_DIR, 'aws.json')) as f:
+    secrets = json.loads(f.read())
+# AWS json 가져오기 위한 장치 여기까지
+
+
+def login_page(request):
+    return render(request,'userapp/login.html')
+
+
+def sign_up_page(request):
+    return render(request,'userapp/sign-up.html')
