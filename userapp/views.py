92972c0dda0c720c3a1a89661617a397a0643711
JerryKim <ecec1023@gmail.com>
2022-03-17 15:36:57 +0900

    수정 - aws.json 디폴트값 설정 문제? 유저 뷰스에서 우선 주석처리!

--- userapp/views.py
+++ userapp/views.py
@@ -1,18 +1,16 @@
-import json
-# AWS json 가져오기 위한 장치
-import os
-from pathlib import Path
-
+from django.shortcuts import render, redirect
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import redirect, render
-
 # from user.models import UserModel
 
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-with open(os.path.join(BASE_DIR, 'aws.json')) as f:
-    secrets = json.loads(f.read())
+# AWS json 가져오기 위한 장치
+# import os
+# import json
+# from pathlib import Path
+#
+# BASE_DIR = Path(__file__).resolve().parent.parent
+# with open(os.path.join(BASE_DIR, 'aws.json')) as f:
+#     secrets = json.loads(f.read())
 # AWS json 가져오기 위한 장치 여기까지
 
 
