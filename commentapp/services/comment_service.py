897ce4432faf58ff8f9fb037a2a64bac87ce67ba
sy <phzzz@naver.com>
2022-03-18 21:23:08 +0900

    add test_when_article_user_and_comment_are_deleteed_at_the_same_time

--- commentapp/services/comment_service.py
+++ commentapp/services/comment_service.py
@@ -1,10 +1,10 @@
 from commentapp.models import Comment
 
 
-def create_comment(article, user, content):
+def create_comment(article_id, user_id, content):
     return Comment.objects.create(
-        article=article,
-        user=user,
+        article_id=article_id,
+        user_id=user_id,
         content=content
     )
 
@@ -13,3 +13,8 @@ def update_comment(comment_id, content):
     target_comment = Comment.objects.get(id=comment_id)
     target_comment.content = content
     target_comment.save()
+
+
+def delete_comment(comment_id):
+    target_comment = Comment.objects.get(id=comment_id)
+    target_comment.delete()
--- commentapp/tests/test_comment.py
+++ commentapp/tests/test_comment.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.test import TestCase
 
 # Given
@@ -5,7 +6,7 @@ from django.test import TestCase
 # Then
 # expect
 from commentapp.models import Author, Comment, Post
-from commentapp.services.comment_service import create_comment, update_comment
+from commentapp.services.comment_service import create_comment, update_comment, delete_comment
 
 
 class TestView(TestCase):
@@ -51,7 +52,7 @@ class TestView(TestCase):
         article.delete()
 
         # Expectdd
-        self.assertEqual(0,len(Comment.objects.all()))
+        self.assertEqual(0, len(Comment.objects.all()))
 
     def test_comment_can_delete_by_deleting_user(self):
         # Given
@@ -65,3 +66,35 @@ class TestView(TestCase):
 
         # Expectdd
         self.assertEqual(0, len(Comment.objects.all()))
+
+    def test_comment_delete(self):
+        # Given
+        user = Author.objects.create(name="test_name")
+        article = Post.objects.create(title="test_title")
+        content = 'test'
+        comment = create_comment(article, user, content)
+
+        # When
+        delete_comment(comment.id)
+
+        # Expectdd
+        self.assertEqual(0, len(Comment.objects.all()))
+
+    def test_when_article_user_and_comment_are_deleteed_at_the_same_time(self):
+        # Given
+        user = Author.objects.create(name="test_name")
+        article = Post.objects.create(title="test_title")
+        content = 'test'
+        comment = create_comment(article, user, content)
+
+        # When
+        user.delete()
+        article.delete()
+
+
+        # Expect
+        with self.assertRaises(ObjectDoesNotExist):
+            delete_comment(comment.id)
+
+
+
