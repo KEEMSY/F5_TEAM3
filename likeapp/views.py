df062662d5041588bdfad40cd58c95bf9feb26fb
DaheePark-00 <96563134+DaheePark-00@users.noreply.github.com>
2022-03-22 22:25:43 +0900

    Update views.py

--- likeapp/views.py
+++ likeapp/views.py
@@ -9,13 +9,12 @@ from likeapp.services.like_service import (do_article_like, do_comment_like,
 
 def click_article_like(request, article_id):
     user = request.user.is_authenticated
-    user_id = Author.objects.get(user_id=request.user)
     if user:
         if request.method == "POST":
-            do_article_like(user_id, article_id)
+            do_article_like(request.user.id, article_id)
             return JsonResponse({'msg': '좋아요'}, status=200)
         elif request.method == "DELETE":
-            undo_article_like(user_id, article_id)
+            undo_article_like(request.user.id, article_id)
             return JsonResponse({'msg': '좋아요 취소'}, status=200)
 
     else:
@@ -24,13 +23,12 @@ def click_article_like(request, article_id):
 
 def click_comment_like(request, comment_id):
     user = request.user.is_authenticated
-    user_id = Author.objects.get(user_id=request.user)
     if user:
         if request.method == "POST":
-            do_comment_like(user_id, comment_id)
+            do_comment_like(request.user.id, comment_id)
             return JsonResponse({'msg': '좋아요'}, status=200)
         elif request.method == "DELETE":
-            undo_comment_like(user_id, comment_id)
+            undo_comment_like(request.user.id, comment_id)
 
             return JsonResponse({'msg': '좋아요 취소'}, status=200)
 
