c0cd0a65beabc5de84fb85803734ec40268336d2
DaheePark-00 <96563134+DaheePark-00@users.noreply.github.com>
2022-03-22 21:59:24 +0900

    Update views.py
    필요없는 쿼리 제거

--- bookmarkapp/views.py
+++ bookmarkapp/views.py
@@ -8,13 +8,12 @@ from likeapp.models import Author
 
 def click_bookmark(request, article_id):
     user = request.user.is_authenticated
-    user_id = Author.objects.get(user_id=request.user)
     if user:
         if request.method == "POST":
-            do_bookmark(user_id, article_id)
+            do_bookmark(request.user.id, article_id)
             return JsonResponse({'msg': '스크랩'}, status=200)
         elif request.method == "DELETE":
-            undo_bookmark(user_id, article_id)
+            undo_bookmark(request.user.id, article_id)
             return JsonResponse({'msg': '스크랩 취소'}, status=200)
 
     else:
