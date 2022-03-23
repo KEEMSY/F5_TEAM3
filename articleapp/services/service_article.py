efd1d9d56f523ea0aa534f4cb97119bd4325afa6
guswns4637 <guswns4637@naver.com>
2022-03-23 17:05:40 +0900

    test_read_all_article

--- articleapp/services/service_article.py
+++ articleapp/services/service_article.py
@@ -3,3 +3,14 @@ from articleapp.models import Article
 def create_article(title, user_id, content, category):
     return Article.objects.create(title=title, user=user_id, content=content, category=category)
 
+#전체, 게시판 별로, 하나만 불러오기
+def read_all_article():
+    return Article.objects.all().order_by('-id')
+
+
+
+
+
+
+
+
--- articleapp/tests/test_service_article.py
+++ articleapp/tests/test_service_article.py
@@ -5,10 +5,7 @@
 import self as self
 from django.test import TestCase
 from articleapp.models import Article, Author
-from articleapp.services.service_article import create_article
-
-#together
-
+from articleapp.services.service_article import create_article, read_all_article
 
 
 class TestView(TestCase):
@@ -23,13 +20,27 @@ class TestView(TestCase):
         # When
         article=create_article(title,user,content,category)
 
-        # Then
-
         # expect
         self.assertIsNotNone(Article.id)
         self.assertEqual(user.id,article.id)
 
+    def test_read_all_article(self):
+        # Given
+        user=Author.objects.create(name='test')
 
+        # When
+        article1 = create_article('title', user, 'content', 'category1')
+        article2 = create_article('title', user, 'content', 'category2')
+        article3 = create_article('title', user, 'content', 'category3')
+
+        # Then
+        article_list = read_all_article().values()
+        latest_article_list = [article3, article2, article1]
+
+        # expect
+        self.assertEqual(len(article_list), 3)
+        for idx in range(len(article_list)):
+            self.assertEqual(article_list[idx]['category'], latest_article_list[idx].category)
 
 
 
