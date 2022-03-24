52c3bb644b68ead1c5c7877fa07e6aef6d41b6c8
JerryKim <ecec1023@gmail.com>
2022-03-24 10:19:22 +0900

    수정 - 채용정보 관련 url 수정 및 보완 / 필요없는 경로 수정, rull based 로 처리시 pk값을 받아올 필요가 없어서 삭제 / career-detail 페이지도 필요 없어보임

--- careerapp/urls.py
+++ careerapp/urls.py
@@ -1,12 +1,12 @@
-
 from django.contrib import admin
 from django.urls import path
 
 from careerapp import views
 
+
 app_name = 'careerapp'
 
 urlpatterns = [
     path('', views.show_job, name='job'),
-    path('<int:pk>/', views.show_job_detail, name="detail")
 ]
+
