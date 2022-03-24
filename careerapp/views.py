e6d83d070fe382c79b8dde8726c6de0e372a8a1a
JerryKim <ecec1023@gmail.com>
2022-03-24 10:21:15 +0900

    수정 - DB에 있는 채용정보 뿌려주는 로직 / 권한받은(로그인한) 유저의 skill(Python,Java,C# 등등)에 맞게 뿌려주는 예외처리해서 렌더

--- careerapp/views.py
+++ careerapp/views.py
@@ -1,12 +1,55 @@
+from django.contrib import auth
 from django.shortcuts import render
-
+from careerapp.models import Career, Author
+from django.core.paginator import Paginator
 # Create your views here.
 
 
 def show_job(request):
-    return render(request,'career.html')
+    login_user = auth.authenticate(request)
+    # print(me_username)
+    if login_user is not None:
+        target_user_id = request.user.id
+        target_user = Author.objects.get(pk=target_user_id)
+        skills = target_user.skill
+
+        if len(skills) >= 2:
+            for skill in skills:
+
+                careers = Career.objects.filter(skills=skill).order_by("id")  # 모든 데이터 조회, id +순으로 해야 최신업데이트된게 위로 나옴.
+                # skills = Author.objects.get(skill=skill)
+                # context에 모든 취업 정보를 저장
+
+                paginator = Paginator(careers, 9)
+                page = int(request.GET.get('page', 1))
+                board_list = paginator.get_page(page)
+
+                context = {'careers': careers, 'board_list': board_list}
+            return render(request, 'career.html', context)
+        else:
+            skill = skills
+            careers = Career.objects.filter(skills=skill).order_by("id")  # 받아온 스킬이 없으므로 python 관련 취업정보만 나오게 설정
+            paginator = Paginator(careers, 9)
+            page = int(request.GET.get('page', 1))
+            board_list = paginator.get_page(page)
+
+            context = {'careers': careers, 'board_list': board_list}
+            return render(request, 'career.html', context)
+
+    else:
+        skill = 'python'
+        careers = Career.objects.filter(skills=skill).order_by("id")  # 받아온 스킬이 없으므로 python 관련 취업정보만 나오게 설정
+        paginator = Paginator(careers, 9)
+        page = int(request.GET.get('page', 1))
+        board_list = paginator.get_page(page)
+
+        context = {'careers': careers, 'board_list': board_list}
+        return render(request, 'career.html', context)
+
+
+
 
 
 # 수정해야하는 부분
-def show_job_detail(request):
-    return render(request,'career-details.html')
+# def show_job_detail(request):
+#     return reverse(request, 'career-details.html')
