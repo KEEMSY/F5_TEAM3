e940355f968e287c136647e8e1c0fadef66fba3b
JerryKim <ecec1023@gmail.com>
2022-03-24 10:15:21 +0900

    추가 - installed app에 careerapp 추가 / 수정 rds 연결을 위한 세팅으로 설정

--- TEAM3_F5_coFI/settings.py
+++ TEAM3_F5_coFI/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     'commentapp',
     'likeapp',
     'bookmarkapp',
+    'careerapp',
 
 
 ]
@@ -73,13 +74,13 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'TEAM3_F5_coFI.urls'
 
 # aws.json 가져와서 버킷,db 접근권한 주기
-# with open(os.path.join(BASE_DIR, 'aws.json')) as f:
-#     secrets = json.loads(f.read())
+with open(os.path.join(BASE_DIR, 'aws.json')) as f:
+    secrets = json.loads(f.read())
 
-# AWS_ACCESS_KEY_ID = secrets['AWS']['ACCESS_KEY_ID']
-# AWS_SECRET_ACCESS_KEY = secrets['AWS']['SECRET_ACCESS_KEY']
-# AWS_STORAGE_BUCKET_NAME = secrets['AWS']['STORAGE_BUCKET_NAME']
-# AWS_DEFAULT_ACL = 'public-read'
+AWS_ACCESS_KEY_ID = secrets['AWS']['ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = secrets['AWS']['SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = secrets['AWS']['STORAGE_BUCKET_NAME']
+AWS_DEFAULT_ACL = 'public-read'
 
 TEMPLATES = [
     {
@@ -107,27 +108,28 @@ WSGI_APPLICATION = 'TEAM3_F5_coFI.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 # 로컬 db 연결
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-# AWS db 연결
 # DATABASES = {
 #     'default': {
-#         'ENGINE': secrets['default']['ENGINE'],
-#         'NAME': secrets['default']['NAME'],
-#         'USER': secrets['default']['USER'],
-#         'PASSWORD': secrets['default']['PASSWORD'],
-#         'HOST': secrets['default']['HOST'],
-#         'PORT': secrets['default']['PORT'],
-#         'OPTIONS': {
-#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-#         },
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+# AWS db 연결
+DATABASES = {
+    'default': {
+        'ENGINE': secrets['default']['ENGINE'],
+        'NAME': secrets['default']['NAME'],
+        'USER': secrets['default']['USER'],
+        'PASSWORD': secrets['default']['PASSWORD'],
+        'HOST': secrets['default']['HOST'],
+        'PORT': secrets['default']['PORT'],
+        'OPTIONS': {
+                'charset': 'utf8mb4',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        },
+    }
+}
 
 
 # Password validation
