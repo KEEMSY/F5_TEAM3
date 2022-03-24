8cfe31f5181212c0491d299e15ebe4c13b3f6908
김 지용 <74017167+JerryKim1023@users.noreply.github.com>
2022-03-25 00:25:20 +0900

    Update views.py
    테스트가 왜?,,, 수정해서 다시 올립니다! 제발 해피패스!

--- careerapp/views.py
+++ careerapp/views.py
@@ -1,4 +1,3 @@
-from django.contrib import auth
 from django.shortcuts import render
 from careerapp.models import Career, Author
 from django.core.paginator import Paginator
@@ -6,12 +5,13 @@ from django.core.paginator import Paginator
 
 
 def show_job(request):
+
     if request.user.is_authenticated:
         target_user_id = request.user.id
         target_user = Author.objects.get(pk=target_user_id)
         skills = target_user.skill
 
-       if len(skills) >= 2:
+        if len(skills) >= 2:
             careers = Career.objects.filter(id=-1) #careers 에 빈 쿼리셋값을 정의하고 싶은데 정의할 방법이 없어 도저히 가져올 수 없는 id=-1로 필터해서 빈값을 넣어줌.
             for skill in skills:
                 career = Career.objects.filter(skills=skill).order_by("id")  # 모든 데이터 조회, id +순으로 해야 최신업데이트된게 위로 나옴.
@@ -19,11 +19,13 @@ def show_job(request):
                 # skills = Author.objects.get(skill=skill)
                 # context에 모든 취업 정보를 저장
 
-                paginator = Paginator(careers, 9)
-                page = int(request.GET.get('page', 1))
-                board_list = paginator.get_page(page)
+            paginator = Paginator(careers, 9)
+            page = int(request.GET.get('page', 1))
+            board_list = paginator.get_page(page)
 
-                context = {'careers': careers, 'board_list': board_list}
+            context = {'careers': careers, 'board_list': board_list}
+            print(careers)
+            print(paginator)
             return render(request, 'career.html', context)
         else:
             skill = skills
@@ -36,13 +38,11 @@ def show_job(request):
             return render(request, 'career.html', context)
 
     else:
-        skill = 'python'
+        skill = 'Python'
         careers = Career.objects.filter(skills=skill).order_by("id")  # 받아온 스킬이 없으므로 python 관련 취업정보만 나오게 설정
         paginator = Paginator(careers, 9)
         page = int(request.GET.get('page', 1))
         board_list = paginator.get_page(page)
 
-
         context = {'careers': careers, 'board_list': board_list}
         return render(request, 'career.html', context)
-
