775b2c2dd537612d2cea675c5d9a09d194758057
guswns4637 <guswns4637@naver.com>
2022-03-25 03:13:40 +0900

    add category

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
 
     # path('', views.home, name='home'), #페이지네이터
     path('', views.article_read, name='home'),
-    path('<str:pk>', views.board_list, name='board_list'),
+    # path('<str:pk>', views.board_list, name='board_list'),
     path('<int:Article_id>', views.detail_read, name='detail_read'),
     path('write/', views.move_to_write, name='move_to_write'),
     path('write/write_Article', views.write_Article, name='write_Article'),
--- commentapp/views.py
+++ commentapp/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-from articleapp.models import Article
+# from articleapp.models import Article
 # Create your views here.
 
 
-
 def show_home(request):
-    article = Article.objects.all()
-    return render(request, 'base.html', {'article':article})
+    # article = Article.objects.all()
+    # article.category
+    return render(request, 'base.html')
 
 
 
