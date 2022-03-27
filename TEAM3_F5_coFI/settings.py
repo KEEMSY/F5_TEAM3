abbd4b28ca984943fff73a910a0cd4387b0f8133
JerryKim <ecec1023@gmail.com>
2022-03-27 16:54:25 +0900

    추가,수정 - Home front ui 1차 작업 완료

--- TEAM3_F5_coFI/settings.py
+++ TEAM3_F5_coFI/settings.py
@@ -81,13 +81,13 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'TEAM3_F5_coFI.urls'
 
 # aws.json 가져와서 버킷,db 접근권한 주기
-# with open(os.path.join(BASE_DIR, 'aws.json')) as f:
-#     secrets = json.loads(f.read())
-#
-# AWS_ACCESS_KEY_ID = secrets['AWS']['ACCESS_KEY_ID']
-# AWS_SECRET_ACCESS_KEY = secrets['AWS']['SECRET_ACCESS_KEY']
-# AWS_STORAGE_BUCKET_NAME = secrets['AWS']['STORAGE_BUCKET_NAME']
-# AWS_DEFAULT_ACL = 'public-read'
+with open(os.path.join(BASE_DIR, 'aws.json')) as f:
+    secrets = json.loads(f.read())
+
+AWS_ACCESS_KEY_ID = secrets['AWS']['ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = secrets['AWS']['SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = secrets['AWS']['STORAGE_BUCKET_NAME']
+AWS_DEFAULT_ACL = 'public-read'
 
 TEMPLATES = [
     {
@@ -120,16 +120,16 @@ WSGI_APPLICATION = 'TEAM3_F5_coFI.wsgi.application'
 
 pymysql.install_as_MySQLdb()  # 이것을 실행하면 pymysql을 사용하면서 mysql클라이언트를 실행하는 것처럼 됨
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': "TEAM3",
-        'USER': "root",
-        'PASSWORD': "2349",
-        'HOST' : "localhost",
-        'PORT' : "3306"
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': "TEAM3",
+#         'USER': "root",
+#         'PASSWORD': "2349",
+#         'HOST' : "localhost",
+#         'PORT' : "3306"
+#     }
+# }
 
 # # 로컬 db 연결
 # DATABASES = {
@@ -140,20 +140,20 @@ DATABASES = {
 # }
 
 # AWS db 연결
-# DATABASES = {
-#     'default': {
-#         'ENGINE': secrets['default']['ENGINE'],
-#         'NAME': secrets['default']['NAME'],
-#         'USER': secrets['default']['USER'],
-#         'PASSWORD': secrets['default']['PASSWORD'],
-#         'HOST': secrets['default']['HOST'],
-#         'PORT': secrets['default']['PORT'],
-#         'OPTIONS': {
-#                 'charset': 'utf8mb4',
-#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-#         },
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': secrets['default']['ENGINE'],
+        'NAME': secrets['default']['NAME'],
+        'USER': secrets['default']['USER'],
+        'PASSWORD': secrets['default']['PASSWORD'],
+        'HOST': secrets['default']['HOST'],
+        'PORT': secrets['default']['PORT'],
+        'OPTIONS': {
+                'charset': 'utf8mb4',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        },
+    }
+}
 
 
 # Password validation
