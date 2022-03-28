680ff10bd0aebff856fa2b6459dd3a2a25fd1c81
JerryKim <ecec1023@gmail.com>
2022-03-29 02:01:05 +0900

    테스트 통과를 위해 세팅스파이 다시 주석처리....

--- TEAM3_F5_coFI/settings.py
+++ TEAM3_F5_coFI/settings.py
@@ -81,13 +81,13 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'TEAM3_F5_coFI.urls'
 
 # aws.json 가져와서 버킷,db 접근권한 주기
-with open(os.path.join(BASE_DIR, 'aws.json')) as f:
-    secrets = json.loads(f.read())
-
-AWS_ACCESS_KEY_ID = secrets['AWS']['ACCESS_KEY_ID']
-AWS_SECRET_ACCESS_KEY = secrets['AWS']['SECRET_ACCESS_KEY']
-AWS_STORAGE_BUCKET_NAME = secrets['AWS']['STORAGE_BUCKET_NAME']
-AWS_DEFAULT_ACL = 'public-read'
+# with open(os.path.join(BASE_DIR, 'aws.json')) as f:
+#     secrets = json.loads(f.read())
+#
+# AWS_ACCESS_KEY_ID = secrets['AWS']['ACCESS_KEY_ID']
+# AWS_SECRET_ACCESS_KEY = secrets['AWS']['SECRET_ACCESS_KEY']
+# AWS_STORAGE_BUCKET_NAME = secrets['AWS']['STORAGE_BUCKET_NAME']
+# AWS_DEFAULT_ACL = 'public-read'
 
 TEMPLATES = [
     {
