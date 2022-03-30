1eb01801bc073309548c87a96206cad4406dafd2
hayongwoon <a01046641857@gmail.com>
2022-03-30 12:01:10 +0900

    주석처리로 코드 부연설명 추가

--- userapp/views.py
+++ userapp/views.py
@@ -11,11 +11,11 @@ from .forms import ProfileForm, UserForm
 
 
 def signup_view(request):
-    if request.user.is_authenticated:
-        return redirect('/')
+    if request.user.is_authenticated: #유저가 로그인이 된 상태라면, signup url 접근을 막는 예외 처리
+        return redirect(reverse('home'))
 
     form = forms.SignUpForm()
-    if request.method == "POST":
+    if request.method == "POST": #post 요청일 때, 이메일과 패스워드를 forms.py를 거쳐 유효성 검사를 마친다.
         form = forms.SignUpForm(request.POST)
         if form.is_valid():
             form.save()
@@ -23,29 +23,33 @@ def signup_view(request):
             password = form.cleaned_data.get("password", '')
 
             user = authenticate(request, email=email, password=password)
+            #athenticate 함수는 메일과 패스워드와 같은 credentials 인자들을 받고 이메일(ID)의 객체를 object.get(username=email)
+            # **우리의 서비스는 이메일로 아이디를 받기 때문에 모델에서 username 필드를 이메일로 커스터마이징 했다.
+            # DoesNotExist 에러이면 return user
+            # 객체가 존재한다면 return None
             if user is not None:
-                login(request, user)
-                return redirect('/')
+                auth.login(request, user)
+                return redirect('/')  #로그인 후 홈 화면으로...(**현재 페이지로 가는 걸로 업데이틀 필요)
             else:
                 return render(request, "userapp/sign-up.html", {"form": form})
 
     return render(request, "userapp/sign-up.html", {"form": form})
 
 def login_view(request):
-    if request.user.is_authenticated:
+    if request.user.is_authenticated: #유저가 로그인이 된 상태라면, signup url 접근을 막는 예외 처리
         return redirect(reverse('home'))
 
     if request.method == "POST":
-        form = forms.LoginForm(request.POST)
+        form = forms.LoginForm(request.POST) #POST 정보를 forms.py에 작성한 LoginForm에 보내고, 검증을 거친 데이터나 해당하는 에러를 받아온다.
         email = request.POST.get("email", '')
         password = request.POST.get("password", '')
 
         get_user = auth.authenticate(request, email=email, password=password)
         if get_user is not None:
             auth.login(request, get_user)
-            return redirect("/")
+            return redirect("/") # signup 과 마찬가지로 로그인 후 최근 활동한 페이지로 이동할 수 있도록 업데이트 필요
         else:
-            return render(request, "userapp/login.html", {"form": form})
+            return render(request, "userapp/login.html", {"form": form}) #데이터 유호성 검사를 실패한 에러 메시지를 보내준다.
 
     elif request.method == "GET":
         form = forms.LoginForm()
@@ -87,6 +91,7 @@ def get_profile(request, pk):
 def update_profile(request, pk):
     u = User.objects.get(id=pk)  # 로그인중인 사용자 객체를 얻어옴
     user_form = UserForm(request.POST, instance=u)  # 기존의 것의 업데이트하는 것 이므로 기존의 인스턴스를 넘겨줘야한다. 기존의 것을 가져와 수정하는 것
+                        #request.POST 는 키로 전송된 자료에 접근할 수 있도록 해주는 사전과 같은 객체, request.POST[key]에 해당하는 value는 항상 문자열! 만약, 해당하는 key의 value가 없다면 keyerror를 반환
 
     # User 폼
     if user_form.is_valid():
@@ -100,7 +105,11 @@ def update_profile(request, pk):
 
     # Profile 폼
     if profile_form.is_valid():
-        profile = profile_form.save(commit=False)  # 기존의 것을 가져와 수정하는 경우가 아닌 새로 만든 경우 user를 지정해줘야 하므로
+        profile = profile_form.save(commit=False)  # 기존의 것을 가져와 수정하는 경우가 아닌 새로 만든 경우 user를 지정해줘야 하므로 commit=False를 통해 우선 디비 저장을 바로 안하고 유저를 지정 후에 디비에 저장
+        #form.save()를 사용 시 자동적으로 DB에 내용이 저장되고 반영됩니다.
+        #여기서 DB 저장 여부를 commit=True, False와 같은 flag를 통해 지정해줄 수 있습니다.
+        #commit=False는 DB에 반영하지 않는 것을 의미합니다.
+
         profile.user = u
         profile.save()
 
