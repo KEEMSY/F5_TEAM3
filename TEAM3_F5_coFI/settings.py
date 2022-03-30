bef3136f5cb34b411077ec382720e238c5a42c33
JerryKim <ecec1023@gmail.com>
2022-03-31 03:00:57 +0900

    제발...

--- TEAM3_F5_coFI/settings.py
+++ TEAM3_F5_coFI/settings.py
@@ -196,7 +196,7 @@ USE_I18N = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 # 배포용
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [
    os.path.join(BASE_DIR, "static"),
 ]
