0575aa527bae0e46fc0ca3b5a8ff3d5c61bd0e22
JerryKim <ecec1023@gmail.com>
2022-03-31 12:00:27 +0900

    서버 배포 완료 설정

--- TEAM3_F5_coFI/settings.py
+++ TEAM3_F5_coFI/settings.py
@@ -39,9 +39,9 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['cofi-f5.com']
 
 # Application definition
 
