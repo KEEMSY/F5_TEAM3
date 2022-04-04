086b1734fce05e474f204fd8222aa1d6e44ad7d2
guswns4637 <guswns4637@naver.com>
2022-04-04 14:11:33 +0900

    css edit

--- TEAM3_F5_coFI/settings.py
+++ TEAM3_F5_coFI/settings.py
@@ -86,8 +86,8 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'TEAM3_F5_coFI.urls'
 
 # aws.json 가져와서 버킷,db 접근권한 주기
-# with open(os.path.join(BASE_DIR, 'aws.json')) as f:
-#     secrets = json.loads(f.read())
+with open(os.path.join(BASE_DIR, 'aws.json')) as f:
+    secrets = json.loads(f.read())
 #
 # AWS_ACCESS_KEY_ID = secrets['AWS']['ACCESS_KEY_ID']
 # AWS_SECRET_ACCESS_KEY = secrets['AWS']['SECRET_ACCESS_KEY']
@@ -124,16 +124,16 @@ WSGI_APPLICATION = 'TEAM3_F5_coFI.wsgi.application'
 
 pymysql.install_as_MySQLdb()  # 이것을 실행하면 pymysql을 사용하면서 mysql클라이언트를 실행하는 것처럼 됨
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': "TEAM3",
-        'USER': "root",
-        'PASSWORD': "2349",
-        'HOST' : "localhost",
-        'PORT' : "3306"
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': "TEAM3",
+#         'USER': "root",
+#         'PASSWORD': "2349",
+#         'HOST' : "localhost",
+#         'PORT' : "3306"
+#     }
+# }
 
 # # 로컬 db 연결
 # DATABASES = {
@@ -144,20 +144,20 @@ DATABASES = {
 # }
 
 # AWS db 연결
-# DATABASES = {
-#     'default': {
-#         'ENGINE': secrets['default']['ENGINE'],
-#         'NAME': secrets['default']['NAME'],
-#         'USER': secrets['default']['USER'],
-#         'PASSWORD': secrets['default']['PASSWORD'],
-#         'HOST': secrets['default']['HOST'],
-#         'PORT': secrets['default']['PORT'],
-#         'OPTIONS': {
-#                 'charset': 'utf8mb4',
-#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-#         },
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': secrets['default']['ENGINE'],
+        'NAME': secrets['default']['NAME'],
+        'USER': secrets['default']['USER'],
+        'PASSWORD': secrets['default']['PASSWORD'],
+        'HOST': secrets['default']['HOST'],
+        'PORT': secrets['default']['PORT'],
+        'OPTIONS': {
+                'charset': 'utf8mb4',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        },
+    }
+}
 
 
 # Password validation
@@ -217,8 +217,8 @@ LOGIN_REDIRECT_URL = '/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 
-try:
-    from TEAM3_F5_coFI.local_settings import *
-except ImportError:
-    pass
+# try:
+#     from TEAM3_F5_coFI.local_settings import *
+# except ImportError:
+#     pass
 
