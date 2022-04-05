0130d4e42c174a9d6764334f3733024fa76178bd
JerryKim <ecec1023@gmail.com>
2022-04-06 01:17:52 +0900

    수정 - 404페이지 /404로 url 경로 추가  /   게시글 뿌려주는 기본세팅 추가(미완성/머지 후 프론트 분리해서 사용할 예정) /  채용정보 뿌려주는 부분 조건문 수정

--- TEAM3_F5_coFI/views.py
+++ TEAM3_F5_coFI/views.py
@@ -2,11 +2,14 @@ from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
+from articleapp.models import Article
 from careerapp.models import Career, News
 from userapp.models import Profile, User
 
 
 def show_home(request):
+    article_list = Article.objects.all().order_by("article_hits")
+
     news = News.objects.all().order_by("id")  # 모든 데이터 조회, id +순으로 해야 최신업데이트된게 위로 나옴.
     target_user_id = request.user.id
     target_user_profile = Profile.objects.filter(id=target_user_id)
@@ -20,7 +23,7 @@ def show_home(request):
     board_lists.append(board)
 
     # 로그인했고, 로그인 한 사람이 스킬까지 있다면,
-    if request.user.is_authenticated and target_user_profile.get().skill is not None:
+    if request.user.is_authenticated:
         skills = []
         for i in target_user_profile:
             skills.append(i.skill)
@@ -33,10 +36,10 @@ def show_home(request):
             board = paginator.get_page(page)
             board_lists.append(board)
 
-        context = {'board_lists': board_lists, "news": news}
+        context = {'board_lists': board_lists, "news": news, "article_list": article_list}
         return render(request, 'base.html', context)
 
-    context = {'board_lists': board_lists, "news": news}
+    context = {'board_lists': board_lists, "news": news, "article_list": article_list}
     return render(request, 'base.html', context)
     
 
@@ -56,6 +59,8 @@ def show_article_list_tip(request):
 def show_article_write(request):
     return render(request, 'articleapp/origin_article_write.html')
 
+def show_404page(request):
+    return render(request, '404page.html')
 
 
 
