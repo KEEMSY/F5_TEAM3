07889157f4a602c897aa1b2e0db07c387ac97f3b
KEEMSY <phzzz@naver.com>
2022-04-06 13:25:35 +0900

    update ajax in article_detail.html

--- commentapp/views.py
+++ commentapp/views.py
@@ -14,7 +14,13 @@ class CommentView(View):
     def post(self, request):
         try:
             comment = create_comment(article_id=request.POST['article_id'], user_id=request.user.id, content=request.POST['content'])
-            return HttpResponse(status=200)
+            print(comment.created_at.strftime('%Y년 %m월 %d일 %H:%M'))
+            date = {
+                'username': comment.user.username,
+                'date': comment.created_at.strftime('%Y년 %m월 %d일 %H:%M'),
+                'content': comment.content
+            }
+            return JsonResponse({'comment': date},status=200)
 
         except IntegrityError:
             return JsonResponse({'msg': '게시글이 존재하지 않습니다.'}, status=400)
