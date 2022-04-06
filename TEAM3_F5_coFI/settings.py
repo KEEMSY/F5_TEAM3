468998962de18c9228d15531bf3815f46444ec67
guswns4637 <guswns4637@naver.com>
2022-04-07 00:39:12 +0900

    remember 기능 추가

--- TEAM3_F5_coFI/settings.py
+++ TEAM3_F5_coFI/settings.py
@@ -229,13 +229,13 @@ SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-try:
-    from TEAM3_F5_coFI.deploy_settings import *
-except ImportError:
-    pass
-
 # try:
-#     from TEAM3_F5_coFI.local_settings import *
+#     from TEAM3_F5_coFI.deploy_settings import *
 # except ImportError:
 #     pass
+
+try:
+    from TEAM3_F5_coFI.local_settings import *
+except ImportError:
+    pass
 
