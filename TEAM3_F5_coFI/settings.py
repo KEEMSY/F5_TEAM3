63b745747ee55d2fafeb6705cc9d7d1272a04421
JerryKim <ecec1023@gmail.com>
2022-04-14 09:08:31 +0900

    디플로이세팅 연결 해제 세팅

--- TEAM3_F5_coFI/settings.py
+++ TEAM3_F5_coFI/settings.py
@@ -231,7 +231,7 @@ SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-try:
-    from TEAM3_F5_coFI.deploy_settings import *
-except ImportError:
-    pass
+# try:
+#     from TEAM3_F5_coFI.deploy_settings import *
+# except ImportError:
+#     pass
